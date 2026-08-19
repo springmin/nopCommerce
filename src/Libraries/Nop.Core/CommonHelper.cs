2bf7a143150524dadb19d4b96505a3daf9b29c15
springmin <springmin@hotmail.com>
2026-08-19 21:02:36 +0800

    Build: pin .NET SDK to 11.0.100-dev and fix GeneratedRegex for invariant globalization
    - global.json: use the 11.0.100-dev SDK (the bundled 10.0.110-dev cannot build
      this repository - it is missing workload locator targets)
    - remove the cultureName argument from GeneratedRegex attributes so they work
      in invariant globalization mode (no ICU) on OpenHarmony

--- src/Libraries/Nop.Core/CommonHelper.cs
+++ src/Libraries/Nop.Core/CommonHelper.cs
@@ -13,23 +13,19 @@ public partial class CommonHelper
 {
     #region Fields
 
-    //we use EmailValidator from FluentValidation. So let's keep them sync - https://github.com/JeremySkinner/FluentValidation/blob/master/src/FluentValidation/Validators/EmailValidator.cs
-    protected const string EMAIL_EXPRESSION = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-||_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+([a-z]+|\d|-|\.{0,1}|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])?([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$";
-
-    protected static readonly Regex _emailRegex;
-
-    #endregion
-
-    #region Ctor
-
-    static CommonHelper()
-    {
-        _emailRegex = new Regex(EMAIL_EXPRESSION, RegexOptions.IgnoreCase);
-    }
+    //we use regular expression based on RFC 5322 Official Standard (see https://emailregex.com/)
+    private const string EMAIL_EXPRESSION = @"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$";
 
     #endregion
 
     #region Methods
+
+    /// <summary>
+    /// Get email validation regex
+    /// </summary>
+    /// <returns>Regular expression</returns>
+    [GeneratedRegex(EMAIL_EXPRESSION, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture)]
+    public static partial Regex GetEmailRegex();
 
     /// <summary>
     /// Ensures the subscriber email or throw.
@@ -43,18 +39,16 @@ public partial class CommonHelper
         output = EnsureMaximumLength(output, 255);
 
         if (!IsValidEmail(output))
-        {
             throw new NopException("Email is not valid.");
-        }
 
         return output;
     }
 
     /// <summary>
-    /// Verifies that a string is in valid e-mail format
+    /// Verifies that a string is in valid email format
     /// </summary>
     /// <param name="email">Email to verify</param>
-    /// <returns>true if the string is a valid e-mail address and false if it's not</returns>
+    /// <returns>true if the string is a valid email address and false if it's not</returns>
     public static bool IsValidEmail(string email)
     {
         if (string.IsNullOrEmpty(email))
@@ -62,7 +56,7 @@ public partial class CommonHelper
 
         email = email.Trim();
 
-        return _emailRegex.IsMatch(email);
+        return GetEmailRegex().IsMatch(email);
     }
 
     /// <summary>
@@ -120,9 +114,7 @@ public partial class CommonHelper
 
         var result = str[0..(maxLength - pLen)];
         if (!string.IsNullOrEmpty(postfix))
-        {
             result += postfix;
-        }
 
         return result;
     }
@@ -192,8 +184,8 @@ public partial class CommonHelper
         ArgumentNullException.ThrowIfNull(propertyName);
 
         var instanceType = instance.GetType();
-        var pi = instanceType.GetProperty(propertyName) 
-                 ?? throw new NopException("No property '{0}' found on the instance of type '{1}'.", propertyName, instanceType);
+        var pi = instanceType.GetProperty(propertyName)
+            ?? throw new NopException("No property '{0}' found on the instance of type '{1}'.", propertyName, instanceType);
 
         if (!pi.CanWrite)
             throw new NopException("The property '{0}' on the instance of type '{1}' does not have a setter.", propertyName, instanceType);
@@ -257,24 +249,40 @@ public partial class CommonHelper
     }
 
     /// <summary>
-    /// Convert enum for front-end
+    /// Splits the camel-case word into separate one
     /// </summary>
     /// <param name="str">Input string</param>
-    /// <returns>Converted string</returns>
-    public static string ConvertEnum(string str)
+    /// <returns>Splitted string</returns>
+    public static string SplitCamelCaseWord(string str)
     {
         if (string.IsNullOrEmpty(str))
             return string.Empty;
-        var result = string.Empty;
-        foreach (var c in str)
-            if (c.ToString() != c.ToString().ToLowerInvariant())
-                result += " " + c.ToString();
-            else
-                result += c.ToString();
+
+        var result = str.ToCharArray()
+            .Select(p => p.ToString())
+            .Aggregate(string.Empty, (current, c) => current + (c == c.ToUpperInvariant() ? $" {c}" : c));
 
         //ensure no spaces (e.g. when the first letter is upper case)
         result = result.TrimStart();
+
         return result;
+    }
+
+    /// <summary>
+    /// Converts a string from <c>SNAKE_CASE</c> to <c>PascalCase</c>
+    /// </summary>
+    /// <param name="str">Input string</param>
+    /// <returns>Splitted <c>PascalCase</c></returns>
+    public static string SnakeCaseToPascalCase(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return string.Empty;
+
+        var tempString = str.ToLower().Replace("_", " ");
+
+        return CultureInfo.InvariantCulture.TextInfo
+            .ToTitleCase(tempString)
+            .Replace(" ", string.Empty);
     }
 
     /// <summary>
@@ -308,7 +316,7 @@ public partial class CommonHelper
         DateTime? date = null;
         try
         {
-            date = new DateTime(year.Value, month.Value, day.Value, CultureInfo.CurrentCulture.Calendar);
+            date = new DateTime(year.Value, month.Value, day.Value, CultureInfo.CurrentCulture.DateTimeFormat.Calendar);
         }
         catch { }
         return date;
